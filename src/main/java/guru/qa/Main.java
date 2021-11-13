d29e6d6c9382e99bfac5ce592015df5f708b012f
cbet <npocbet@gmail.com>
2021-11-13 12:50:51 +0300

    hw for "Дополнительное занятие «Основы Java - продолжение». Дмитрий Тучс." is done. Static class, method and fields added. Constructor, getters and setters added too.

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,15 +3,17 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args){
-        Seance first_movie = new Seance(30, "Игла", 100);
+        Seance first_movie = new Seance(120, 30, "Игла", 100);
 
-        first_movie.sellTicket(10);
+        first_movie.setTickets_sold(10); // украли 20 билетов))
         first_movie.sellTicket(5);
         first_movie.sellTicket(20);
+        System.out.println("напоминаем, фильм называется " + first_movie.getMovie_name());
+        System.out.println(first_movie.film.getFilmsInvitation());
 
         first_movie.printInfo();
 
-        Seance second_movie = new Seance(20, "Асса", 50);
+        Seance second_movie = new Seance(150, 20, "Асса", 50);
 
         second_movie.sellTicket(10);
         second_movie.sellTicket(15);
--- src/main/java/guru/qa/Seance.java
+++ src/main/java/guru/qa/Seance.java
@@ -3,29 +3,74 @@ package guru.qa;
 public class Seance {
     int tickets_sold = 0;
     int number_of_seats;
-    String movie_name;
     int ticket_cost;
+    Film film;
 
-    public Seance(int i_number_of_seats, String i_movie_name, int i_ticket_cost) {
+    static class Film {
+        int duration;
+        String filmTitle;
+        public Film(String filmTitleT, int duration){
+            this.filmTitle = filmTitleT;
+            this.duration = duration;
+        }
+
+        public String getFilmsInvitation(){
+            return "Текущий сеанс " + filmTitle;
+        }
+    }
+
+    public Seance(int duration, int i_number_of_seats, String i_movie_name, int i_ticket_cost) {
+        film = new Film(i_movie_name, duration);
         number_of_seats = i_number_of_seats;
-        movie_name = i_movie_name;
         ticket_cost = i_ticket_cost;
         System.out.printf("Начинается продажа билетов на сеанс %s, количество мест %d, цена билета %d\n",
-                movie_name, number_of_seats, ticket_cost);
+                film.filmTitle, number_of_seats, ticket_cost);
     }
 
     public void sellTicket(int number_of_tickets){
         if (tickets_sold + number_of_tickets <= number_of_seats){
             tickets_sold += number_of_tickets;
-            System.out.printf("Продано %d билетов на сеанс %s \n", number_of_tickets, movie_name);
+            System.out.printf("Продано %d билетов на сеанс %s \n", number_of_tickets, film.filmTitle);
         }
         else {
-            System.out.printf("Недостаточно свободных мест на сеанс %s \n", movie_name);
+            System.out.printf("Недостаточно свободных мест на сеанс %s \n", film.filmTitle);
         }
     }
 
     public void printInfo(){
         System.out.printf("На сеанс %s продано %d билетов общей стоимостью %d \n",
-                movie_name, tickets_sold, tickets_sold * ticket_cost);
+                film.filmTitle, tickets_sold, tickets_sold * ticket_cost);
+    }
+
+    public int getTickets_sold() {
+        return tickets_sold;
+    }
+
+    public int getNumber_of_seats() {
+        return number_of_seats;
+    }
+
+    public String getMovie_name() {
+        return film.filmTitle;
+    }
+
+    public int getTicket_cost() {
+        return ticket_cost;
+    }
+
+    public void setTickets_sold(int tickets_sold) {
+        this.tickets_sold = tickets_sold;
+    }
+
+    public void setNumber_of_seats(int number_of_seats) {
+        this.number_of_seats = number_of_seats;
+    }
+
+    public void setMovie_name(String movie_name) {
+        this.film.filmTitle = movie_name;
+    }
+
+    public void setTicket_cost(int ticket_cost) {
+        this.ticket_cost = ticket_cost;
     }
 }
